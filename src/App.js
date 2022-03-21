615f725e4d69e5f978b66dc5ca63a6e177b13d66
Jerry Vohrer <92649050+Jerry-Vrrr@users.noreply.github.com>
2022-03-21 15:22:44 -0600

    Add form file

--- src/App.js
+++ src/App.js
@@ -10,6 +10,17 @@ class App extends Component {
       <main>
       <h1>IdeaBox</h1>
       <p>Hi!</p>
+      <form>
+  <label>
+    Title:
+    <input type="text" name="name" />
+  </label>
+  <label>
+    Description:
+    <input type="text" name="name" />
+  </label>
+  <input type="submit" value="Submit" />
+</form>
             </main>
     )
   }

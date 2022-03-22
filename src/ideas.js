3b3d4c1514dac9872b669d0c5c77652f50b917f2
Jerry Vohrer <92649050+Jerry-Vrrr@users.noreply.github.com>
2022-03-21 19:00:32 -0600

    Create card function and link to ideas function

--- src/ideas.js
+++ src/ideas.js
@@ -2,14 +2,26 @@ import React, { Component } from 'react';
 import "./ideas.css"
 import Card from './Card'
 
-const Idea = () => {
+
+
+const Ideas = ( {ideas} ) => {
+
+  const ideaCards = ideas.map(idea => {
+    return (
+      <Card className ='card-section'
+        title={idea.title}
+        description={idea.description}
+        id={idea.id}
+        key={idea.id}
+      />
+    )
+  })
+
   return (
-    <section className ='card-section'>
-      <Card/>
-      <Card/>
-      <Card/>
-    </section>
+    <div className='ideas-container'>
+      {ideaCards}
+    </div>
   )
 }
 
-export default Idea;
+export default Ideas;

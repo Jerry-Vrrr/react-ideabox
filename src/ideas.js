982606f92c5491d61628d92ab7643239799eadcf
Jerry Vohrer <92649050+Jerry-Vrrr@users.noreply.github.com>
2022-03-21 18:03:11 -0600

    Add card class file and css file

--- src/ideas.js
+++ src/ideas.js
@@ -1,10 +1,13 @@
 import React, { Component } from 'react';
 import "./ideas.css"
+import Card from './Card'
 
 const Idea = () => {
   return (
     <section className ='card-section'>
-      <box className ='card'></box>
+      <Card/>
+      <Card/>
+      <Card/>
     </section>
   )
 }
